8d94358580817b40b50522f5cf2d9e083691e125
Kreideprinz <jbr.abdul@upi.edu>
2021-05-31 22:43:17 +0700

    test, kirain kode w error, taunya postmannya.

--- src/handler.js
+++ src/handler.js
@@ -63,7 +63,6 @@ const addBook = (request, h) => {
       }
     })
     response.code(201)
-
     return response
   }
 
@@ -72,45 +71,156 @@ const addBook = (request, h) => {
     message: 'Buku gagal ditambahkan'
   })
   response.code(500)
-
   return response
 }
 
 const getAllBooks = (request, h) => {
-  const {
-    name,
-    reading,
-    finished
-  } = request.query
+  const { name, reading, finished } = request.query;
 
-  let searchBook
+  let filteredBooks = books;
 
   if (name !== undefined) {
-    searchBook = books.filter((book) => book
-      .name.toLowerCase().includes(name.toLowerCase()))
+    filteredBooks = filteredBooks.filter((book) => book
+      .name.toLowerCase().includes(name.toLowerCase()));
   }
 
   if (reading !== undefined) {
-    searchBook = books.filter((book) => book.reading === !!Number(reading))
+    filteredBooks = filteredBooks.filter((book) => book.reading === !!Number(reading));
   }
 
   if (finished !== undefined) {
-    searchBook = books.filter((book) => book.finished === !!Number(finished))
+    filteredBooks = filteredBooks.filter((book) => book.finished === !!Number(finished));
   }
 
   const response = h.response({
     status: 'success',
     data: {
-      books: searchBook.map((book) => ({
+      books: filteredBooks.map((book) => ({
         id: book.id,
         name: book.name,
-        publisher: book.publisher
-      }))
-    }
-  })
-  response.code(200)
+        publisher: book.publisher,
+      })),
+    },
+  });
+  response.code(200);
 
+  return response;
+};
+
+const getBookById = (request, h) => {
+  const { id } = request.params;
+  const book = books.filter((book) => book.id === id)[0];
+
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+
+  return response;
+};
+
+const editBookById = (request, h) => {
+  const { id } = request.params;
+
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const updatedAt = new Date().toISOString();
+  const index = books.findIndex((n) => n.id === id);
+
+  if (name === undefined) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message:
+        'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (index !== -1) {
+    books[index] = {
+      ...books[index],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      reading,
+      updatedAt,
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui buku. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+}
+
+const deleteBookById = (request, h) => {
+  const { id } = request.params
+  const index = books.findIndex((book) => book.id === id)
+
+  if (index !== -1) {
+    books.splice(index, 1)
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil dihapus'
+    })
+    response.code(200)
+    return response
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal dihapus. Id tidak ditemukan'
+  })
+  response.code(404)
   return response
 }
 
-module.exports = { addBook, getAllBooks }
+module.exports = {
+  addBook,
+  getAllBooks,
+  getBookById,
+  editBookById,
+  deleteBookById
+}
